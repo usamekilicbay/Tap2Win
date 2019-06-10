e6d5b1d81785fb7ed979dec271b34d72c9dbb17c
PapillonW <wvalentine0909@gmail.com>
2019-06-10 23:34:46 +0300

    Main menu finished

---
+++ assets/Scripts/UI/CanvasController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ExtraTools;
+
+public class CanvasController : MonoBehaviour
+{
+    public void MainMenu() { UIManager.Instance.CanvasChanger(CanvasObjects.MainMenu); }
+
+    public void Settings() { UIManager.Instance.CanvasChanger(CanvasObjects.Settings); }
+
+    public void InGame() { UIManager.Instance.CanvasChanger(CanvasObjects.InGame); }
+
+    public void Tutorial() { UIManager.Instance.CanvasChanger(CanvasObjects.Tutorial); }
+}
---
+++ assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace ExtraTools {
+
+    public enum CanvasObjects { MainMenu, Settings, InGame , Tutorial}
+
+    public class UIManager : Singleton<UIManager>
+    {
+        [SerializeField]
+        private GameObject mainMenu;
+
+        [SerializeField]
+        private GameObject settings;
+
+        [SerializeField]
+        private GameObject inGame;
+
+        [SerializeField]
+        private GameObject tutorial;
+
+        public void CanvasChanger(CanvasObjects canvasObjects)
+        {
+            mainMenu.SetActive(canvasObjects == CanvasObjects.MainMenu);
+
+            settings.SetActive(canvasObjects == CanvasObjects.Settings);
+
+            inGame.SetActive(canvasObjects == CanvasObjects.InGame);
+
+            tutorial.SetActive(canvasObjects == CanvasObjects.Tutorial);
+        }
+
+
+
+    }
+}
